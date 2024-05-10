d1481eb4a3e305ccc0518ed2d155ed4f64b99f6a
Fabio Ferreras <fabio_ferreras@hotmail.com>
2024-05-09 23:13:53 -0400

    Add DB Models

--- backend/Calculator/admin.py
+++ backend/Calculator/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Holiday, FundsSetting
 
-# Register your models here.
+
+@admin.register(Holiday)
+class HolidayAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Holiday._meta.get_fields()]
+
+
+@admin.register(FundsSetting)
+class FundsSettingAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in FundsSetting._meta.get_fields()]
--- backend/Calculator/models.py
+++ backend/Calculator/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='')
+    days_lte = models.PositiveIntegerField()
+    days_gt = models.PositiveIntegerField()
+    days_lte_reinvest = models.PositiveIntegerField()
+    days_gt_reinvest = models.PositiveIntegerField()
+    enabled = models.BooleanField(default=True)
+
+
+class Holiday(models.Model):
+    name = models.CharField(max_length=50, blank=False, default='')
+    holiday_date = models.DateField(unique=True)
+
+
+class FundsSetting(models.Model):
+    operational_hour = models.TimeField()
